5a36aa509eff04347fbe5d18d70c8a44fa0febd6
mlwheaton08 <mlwheaton06@gmail.com>
2022-08-20 13:11:23 -0500

    hi

--- scripts/database.js
+++ scripts/database.js
@@ -1,4 +1,4 @@
-const Tables = [
+const tables = [
     {
         id: 1 ,
         menuID: [0,0,0],
@@ -6,7 +6,8 @@ const Tables = [
         reservation: true,
         orderStatus: "complete",
         serverId: 1,
-        guestsNumber: 0 
+        guestsNumber: 0,
+        tipPercent: 0.2
     },
     {
         id: 2 ,
@@ -15,7 +16,8 @@ const Tables = [
         reservation: true,
         orderStatus: "complete",
         serverId: 2,
-        guestsNumber: 0
+        guestsNumber: 0,
+        tipPercent: 0.2
     },
     {
         id: 3,
@@ -24,7 +26,8 @@ const Tables = [
         reservation: true,
         orderStatus: "complete",
         serverId: 3,
-        guestsNumber: 0
+        guestsNumber: 0,
+        tipPercent: 0.2
     },
     {
         id: 4,
@@ -33,7 +36,8 @@ const Tables = [
         reservation: true,
         orderStatus: "complete",
         serverId: 4,
-        guestsNumber: 0
+        guestsNumber: 0,
+        tipPercent: 0.2
     },
     {
         id: 5,
@@ -42,7 +46,8 @@ const Tables = [
         reservation: true,
         orderStatus: "complete",
         serverId: 5,
-        guestsNumber: 0
+        guestsNumber: 0,
+        tipPercent: 0.2
     }
 
 ]
@@ -51,77 +56,92 @@ const menu = [
     {
         id: 1,
         type: "breakfast",
-        menuItem: "Eggs Benedict"
+        menuItem: "Eggs Benedict",
+        price: 30
     },
     {
         id: 2,
         type: "breakfast",
-        menuItem: "Pancakes and Bacon"
+        menuItem: "Crepes and Bacon",
+        price: 25
     },
     {
         id: 3,
         type: "breakfast",
-        menuItem: "Sausage Biscuits and Gravy"
+        menuItem: "Sausage Biscuits and Gravy",
+        price: 95
     },
     {
         id: 4,
         type: "lunch",
-        menuItem: "Cheeseburger"
+        menuItem: "Cheeseburger",
+        price: 5
     },
     {
         id: 5,
         type: "lunch",
-        menuItem: "Chicken Caesar Salad"
+        menuItem: "Chicken Caesar Salad",
+        price: 65
     },
     {
         id: 6,
         type: "lunch",
-        menuItem: "Grilled Steak"
+        menuItem: "Grilled Steak",
+        price: 420
     },
     {
         id: 7,
         type: "dinner",
-        menuItem: "Meat Loaf and Mashed Potatoes"
+        menuItem: "Meat Loaf and Mashed Potatoes",
+        price: 2
     },
     {
         id: 8,
         type: "dinner",
-        menuItem: "Salmon and Yellow Rice"
+        menuItem: "Salmon and Yellow Rice",
+        price: 69
     },
     {
         id: 9,
         type: "dinner",
-        menuItem: "Chicken Teriyaki"
+        menuItem: "Chicken Teriyaki",
+        price: 60
     },
     {
         id: 10,
         type: "drink",
-        menuItem: "Margarita"
+        menuItem: "Margarita",
+        price: 150
     },
     {
         id: 11,
         type: "drink",
-        menuItem: "Water"
+        menuItem: "Water",
+        price: 1000
     },
     {
         id: 12,
         type: "drinks",
-        menuItem: "Buffalo Trace"
+        menuItem: "Buffalo Trace",
+        price: 5
     },
     {
         id: 13,
         type: "drinks",
-        menuItem: "Coors Light"
+        menuItem: "Coors Light",
+        price: 20
     },
     {
         id: 14,
         type: "drinks",
-        menuItem: "Old Fashioned"
+        menuItem: "Old Fashioned",
+        price: 1
     },
     {
         id: 15,
         type: "drinks",
-        menuItem: "Red Blend"
+        menuItem: "Red Blend",
+        price: 60
     }
 ]
 
@@ -148,3 +168,16 @@ const servers = [
         serverName: "Jimmy"
     }
 ]
+
+
+export const tablesCopy = () => {
+    return tables.map(table => ({...table}))
+}
+
+export const menuCopy = () => {
+    return menu.map(menuItem => ({...menuItem}))
+}
+
+export const serversCopy = () => {
+    return servers.map(server => ({...server}))
+}
