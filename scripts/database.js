ca81e88f81d433fa37ede8b083b13a97d92856b9
Chase Burnett <chaseburnett@me.com>
2022-08-20 12:54:36 -0500

    added databas.js

---
+++ scripts/database.js
@@ -0,0 +1,150 @@
+const Tables = [
+    {
+        id: 1 ,
+        menuID: [0,0,0],
+        menuType: "breakfast",
+        reservation: true,
+        orderStatus: "complete",
+        serverId: 1,
+        guestsNumber: 0 
+    },
+    {
+        id: 2 ,
+        menuID: [0,0,0],
+        menuType: "breakfast",
+        reservation: true,
+        orderStatus: "complete",
+        serverId: 2,
+        guestsNumber: 0
+    },
+    {
+        id: 3,
+        menuID: [0,0,0],
+        menuType: "breakfast",
+        reservation: true,
+        orderStatus: "complete",
+        serverId: 3,
+        guestsNumber: 0
+    },
+    {
+        id: 4,
+        menuID: [0,0,0],
+        menuType: "breakfast",
+        reservation: true,
+        orderStatus: "complete",
+        serverId: 4,
+        guestsNumber: 0
+    },
+    {
+        id: 5,
+        menuID: [0,0,0],
+        menuType: "breakfast",
+        reservation: true,
+        orderStatus: "complete",
+        serverId: 5,
+        guestsNumber: 0
+    }
+
+]
+
+const menu = [
+    {
+        id: 1,
+        type: "breakfast",
+        menuItem: "Eggs Benedict"
+    },
+    {
+        id: 2,
+        type: "breakfast",
+        menuItem: "Pancakes and Bacon"
+    },
+    {
+        id: 3,
+        type: "breakfast",
+        menuItem: "Sausage Biscuits and Gravy"
+    },
+    {
+        id: 4,
+        type: "lunch",
+        menuItem: "Cheeseburger"
+    },
+    {
+        id: 5,
+        type: "lunch",
+        menuItem: "Chicken Caesar Salad"
+    },
+    {
+        id: 6,
+        type: "lunch",
+        menuItem: "Grilled Steak"
+    },
+    {
+        id: 7,
+        type: "dinner",
+        menuItem: "Meat Loaf and Mashed Potatoes"
+    },
+    {
+        id: 8,
+        type: "dinner",
+        menuItem: "Salmon and Yellow Rice"
+    },
+    {
+        id: 9,
+        type: "dinner",
+        menuItem: "Chicken Teriyaki"
+    },
+    {
+        id: 10,
+        type: "drink",
+        menuItem: "Margarita"
+    },
+    {
+        id: 11,
+        type: "drink",
+        menuItem: "Water"
+    },
+    {
+        id: 12,
+        type: "drinks",
+        menuItem: "Buffalo Trace"
+    },
+    {
+        id: 13,
+        type: "drinks",
+        menuItem: "Coors Light"
+    },
+    {
+        id: 14,
+        type: "drinks",
+        menuItem: "Old Fashioned"
+    },
+    {
+        id: 15,
+        type: "drinks",
+        menuItem: "Red Blend"
+    }
+]
+
+
+const servers = [
+    {
+        id: 1,
+        serverName: "Mason"
+    },
+    {
+        id: 2,
+        serverName: "Mariana"
+    },
+    {
+        id: 3,
+        serverName: "Ethan"
+    },
+    {
+        id: 4,
+        serverName: "Chase"
+    },
+    {
+        id: 5,
+        serverName: "Jimmy"
+    }
+]
