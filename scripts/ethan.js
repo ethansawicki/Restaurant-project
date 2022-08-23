71384ca4bb399396882e12a15d67fcb66ff8333a
Ethan Sawicki <ethansawicki2797@gmail.com>
2022-08-23 01:29:03 -0500

    made more changes to ethan.sj

--- scripts/ethan.js
+++ scripts/ethan.js
@@ -1,23 +1,33 @@
 import { menuCopy, tablesCopy, serversCopy } from "./database.js"
 
+//is there a way to render the options without pushing to a new array?
 const ordersFunction = () => {
-    const tableOrder = []
-    const menus = menuCopy().filter(menu => {if(menu.type === 'dinner'){return tableOrder.push(menu.menuItem)}})
-    return tableOrder
+    const orders = []
+    const menus = menuCopy().filter(menu => {if(menu.type === 'dinner'){return orders.push(menu.menuItem)}})
+    return orders
+}
+
+const tablesFunction = () => {
+    const tables = tablesCopy().filter(table => {if(table.serverId === 3){return table}})
+    return tables
+}
+
+const serversFunction = () => {
+    const servers = serversCopy().filter(server => {if(server.id === 3){return server}})
+    return servers
 }
 
 const render = () => {
-    const tables = tablesCopy().filter(table => {if(table.serverId === 3){return table}})
-    const servers = serversCopy().filter(server => {if(server.id === 3){return server}})
+    const reservation = new Date(Date.now())
     const appElement = document.querySelector('#ethan')
     let renderToHTML = `<section class="ethan-section">`
-    for (const server of servers) {
-        for (const table of tables) {
+    for (const server of serversFunction()) {
+        for (const table of tablesFunction()) {
             if(server.id === table.serverId) {
                 renderToHTML += `<ul class="ethan-list">`
                 renderToHTML += `<h3 class="ethan-table">Table: ${table.id}</h3>`
                 renderToHTML += `<li>Party Of: ${table.guestsNumber}</li>`
-                renderToHTML += `<li>Table Reservation Date: ${Date()}</li>`
+                renderToHTML += `<li>Table Reservation Date: ${reservation.toLocaleDateString('en-US')}</li>`
                 renderToHTML += `<li>Table Reservation?: ${table.reservation}</li>`
                 renderToHTML += `<li>Order Entree(s):${ordersFunction()}</li>`
                 renderToHTML += `<li>Server: ${server.serverName}</li>`
